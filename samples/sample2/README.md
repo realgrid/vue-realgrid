35f585981ff5d7ae7931e6f1a1caee5af5218437
onlydel <onlydel@realgrid.com>
2018-10-23 12:56:07 +0900

    update sample2

--- samples/sample2/README.md
+++ samples/sample2/README.md
@@ -8,6 +8,10 @@
 
 [`vue cli`](https://cli.vuejs.org/guide/creating-a-project.html#using-the-gui) `init` 를 통해 `webpack-simple` 템플릿 생성
 
+``` bash
+vue init webpack-simple sample2
+```
+
 ## RealGrid 설치
 
 realgrid는 도움말의 [RealGridJS 설치하기](http://help.realgrid.com/tutorial/a1/) 를 보면서 설치한다.
@@ -39,9 +43,39 @@ Vue instance에서 RealGrid 컴포넌트를 사용할 수 있다.
     ...
     <RealGrid grid-id="realgrid1"
       grid-root="/lib"
-      @rendered="gridRendered"
-      class="grid">
+      @rendered="gridRendered">
     </RealGrid>
     ...
 </template>
+```
+
+#### props & events
+
+- grid-id: RealGrid 생성 ID, 여러개의 RealGrid를 한 페이지에서 사용할 경우 개별 ID로 구분해야 한다.
+- grid-root: `assets`폴더가 있는 경로를 입력해야 한다.
+- @rendered: RealGrid가 렌더링된 후 발생하며 `dataProvider` 객체와 `gridView` 객체를 인자로 반환한다.
+
+### script
+
+RealGrid 컴포넌트가 생성되면 `@rendered` 이벤트가 발생한다. 이 이벤트에서 `dataProvider` 와 `gridView` 객체를 받아서 그리드의 기능을 처리할 수 있습니다. 자세한 내용은 [리얼그리드 도움말 사이트](http://help.realgrid.com)와 [데모사이트](http://demo.realgrid.com)를 참고하세요.
+
+``` js
+<script>
+    ...
+    methods: {
+        ...
+        gridRendered(provider, view) {
+            RealGridJS.setRootContext("/");
+
+            this.dataProvider = provider;
+            this.gridView = view;
+
+            // 이벤트 처리
+            this.dataProvider.onRowInserted = this.onRowInserted;
+            this.gridView.onDataCellClicked = this.onDataCellClicked;
+        },
+        ...
+    }
+    ...
+</script>
 ```
--- samples/sample2/src/App.vue
+++ samples/sample2/src/App.vue
@@ -25,80 +25,80 @@
 import RealGrid from '@realgrid/vue-realgrid';
 
 export default {
-  name: 'app',
-  components: {
-    RealGrid
-  },
+    name: 'app',
+    components: {
+        RealGrid
+    },
     data() {
-    return {
-        dataProvider: null,
-        gridView: null,
-        messages: [],
-    }
+        return {
+            dataProvider: null,
+            gridView: null,
+            messages: [],
+        }
     },
     methods: {
-    addMessage(msg) {
-        this.messages.push(`${msg}`);
-    },
-    // 행 추가 이벤트 처리
-    onRowInserted(provider, row) {
-        const values = provider.getRows(row);
-        this.addMessage(`[EVENT] onRowInserted <${values}>`);
-    },
-    // 셀 클릭 이벤트 처리
-    onDataCellClicked(grid, index) {
-        this.addMessage(`[EVENT] onDataCellClicked <${JSON.stringify(index)}>`)
-    },
-    // 그리드가 마운트 될때 매핑
-    gridRendered(provider, view) {
-        RealGridJS.setRootContext("/");
-
-        this.dataProvider = provider;
-        this.gridView = view;
-
-        // 이벤트 처리
-        this.dataProvider.onRowInserted = this.onRowInserted;
-        this.gridView.onDataCellClicked = this.onDataCellClicked;
-    },
-    setFields() {
-        const fields = [
-        {
-            fieldName: "field1"
+        addMessage(msg) {
+            this.messages.push(`${msg}`);
         },
-        {
-            fieldName: "field2"
-        }
-        ];
-        this.dataProvider.setFields(fields);
-    },
-    setColumns() {
-        const columns = [
-        {
-            name: "col1",
-            fieldName: "field1",
-            header : {
-            text: "컬럼1"
-            },
-            width: 200
+        // 행 추가 이벤트 처리
+        onRowInserted(provider, row) {
+            const values = provider.getRows(row);
+            this.addMessage(`[EVENT] onRowInserted <${values}>`);
         },
-        {
-            name: "col2",
-            fieldName: "field2",
-            header : {
-            text: "컬럼2"
+        // 셀 클릭 이벤트 처리
+        onDataCellClicked(grid, index) {
+            this.addMessage(`[EVENT] onDataCellClicked <${JSON.stringify(index)}>`)
+        },
+        // 그리드가 마운트 될때 매핑
+        gridRendered(provider, view) {
+            RealGridJS.setRootContext("/");
+
+            this.dataProvider = provider;
+            this.gridView = view;
+
+            // 이벤트 처리
+            this.dataProvider.onRowInserted = this.onRowInserted;
+            this.gridView.onDataCellClicked = this.onDataCellClicked;
+        },
+        setFields() {
+            const fields = [
+            {
+                fieldName: "field1"
             },
-            width: 200
-        }
-        ];
-        this.gridView.setColumns(columns);
-    },
-    addRow() {
-        const rowCount = this.dataProvider.getRowCount();
-        this.dataProvider.addRow({
-            field1: `Field1 Value:${rowCount}`,
-            field2: `Field2 Value:${rowCount}`,
-        });
-    },
+            {
+                fieldName: "field2"
+            }
+            ];
+            this.dataProvider.setFields(fields);
+        },
+        setColumns() {
+            const columns = [
+            {
+                name: "col1",
+                fieldName: "field1",
+                header : {
+                text: "컬럼1"
+                },
+                width: 200
+            },
+            {
+                name: "col2",
+                fieldName: "field2",
+                header : {
+                text: "컬럼2"
+                },
+                width: 200
+            }
+            ];
+            this.gridView.setColumns(columns);
+        },
+        addRow() {
+            const rowCount = this.dataProvider.getRowCount();
+            this.dataProvider.addRow({
+                field1: `Field1 Value:${rowCount}`,
+                field2: `Field2 Value:${rowCount}`,
+            });
+        },
     }
 }
 </script>
